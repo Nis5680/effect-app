958890890f832f01275f4283a1b6a14276720ff7
Nis5680 <nishitshah5680@gmail.com>
2025-12-11 15:05:30 +0530

    3rd_change

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,11 +8,22 @@ import { Component, effect, signal } from '@angular/core';
 })
 export class App {
   userName = signal('Nishit');
-
+  count= signal(0);
+  displayheading=false;
   constructor() {
     effect(() => {
-      console.log(this.userName());
+      if(this.count()==2){
+        this.displayheading=true
+        setTimeout(() => {
+          this.displayheading=false
+        }, 2000)
+      }else{
+        this.displayheading=false;
+      }
     });
   }
-
+  toggleValue(){
+    this.count.set(this.count()+1);
+    this.displayheading=!this.displayheading;
+  }
 }
